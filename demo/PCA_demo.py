b7beb7db9d7541ef62f40f3181d65ea1a8d52a15
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-05-26 22:20:19 +0900

    Some fixes

--- demo/PCA_demo.py
+++ demo/PCA_demo.py
@@ -6,8 +6,8 @@ sys.path.append('../')
 from loglizer.models import PCA
 from loglizer import dataloader, preprocessing
 
-# struct_log = '../data/HDFS/HDFS_100k.log_structured.csv' # The structured log file
-# label_file = '../data/HDFS/anomaly_label.csv' # The anomaly label file
+struct_log = '../data/HDFS/HDFS_100k.log_structured.csv' # The structured log file
+label_file = '../data/HDFS/anomaly_label.csv' # The anomaly label file
 
 pkl_path = "../../proceeded_data/BGL"
 
